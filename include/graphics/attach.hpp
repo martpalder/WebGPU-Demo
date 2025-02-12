8b8e35b3aeca9f6d03f43345a7acb8dfe69601b5
martpalder <56435324+martpalder@users.noreply.github.com>
2025-02-12 21:02:27 +0200

    Delete include/graphics/attach.hpp

--- include/graphics/attach.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef ATTACH_HPP_INCLUDED
-#define ATTACH_HPP_INCLUDED
-
-#include <webgpu/webgpu.h>
-
-WGPURenderPassColorAttachment createRenderPassColorAttach();
-
-#endif	// ATTACH_HPP_INCLUDED
