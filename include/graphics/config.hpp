ce6603d5fa021e3e98dac3584150635eea3d73e3
martpalder <56435324+martpalder@users.noreply.github.com>
2025-02-12 21:02:42 +0200

    Delete include/graphics/config.hpp

--- include/graphics/config.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef CONFIG_HPP_INCLUDED
-#define CONFIG_HPP_INCLUDED
-
-#include <webgpu/webgpu.h>
-
-WGPUSurfaceConfiguration createSurfConfig(WGPUDevice device);
-
-#endif	// CONFIG_HPP_INCLUDED
