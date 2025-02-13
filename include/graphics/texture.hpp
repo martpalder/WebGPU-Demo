7c9cfc6da37a1f88eaa0ae841b5c6393d331092b
martpalder <56435324+martpalder@users.noreply.github.com>
2025-02-13 17:17:40 +0200

    Delete include/graphics/texture.hpp

--- include/graphics/texture.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef TEXTURE_HPP_INCLUDED
-#define TEXTURE_HPP_INCLUDED
-
-#include <webgpu/webgpu.h>
-
-WGPUTextureView getNextTextureView(const WGPUSurface& surf);
-
-#endif	// TEXTURE_HPP_INCLUDED
